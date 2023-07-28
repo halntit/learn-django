34da4c27b79292c3070699a84bf0132a7c986240
Ha Le <halntit@gmail.com>
2023-07-28 18:00:00 +0100

    update model

--- 1-from-blank/src/products/models.py
+++ 1-from-blank/src/products/models.py
@@ -8,4 +8,5 @@ class Product(models.Model):
     title       = models.CharField(max_length=120) # max_length: required
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=100)
-    summary     = models.TextField(default='')
+    summary     = models.TextField(blank=True)
+    featured    = models.BooleanField(default=False)
